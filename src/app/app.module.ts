aacbb9f8ec36030dc14fd6377ef26641aab8b379
HM-Hadil <hadilhammami670@gmail.com>
2023-11-17 00:49:43 +0100

    adding home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './Home/home-page/home-page.component';
+import { HeaderComponent } from './Home/header/header.component';
+import { FooterComponent } from './Home/footer/footer.component';
+import { AboutComponent } from './Home/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
