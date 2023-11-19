554f38a4b0a28bb2ed0f7cbd71b816d736e676d5
HM-Hadil <hadilhammami670@gmail.com>
2023-11-19 21:59:31 +0100

    updating component consulter poids

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ConsulterPoidsComponent } from './services/consulter-poids/consulter-poids.component';
+import { AppComponent } from './app.component';
+import { HomePageComponent } from './Home/home-page/home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomePageComponent},
+  {path:'consulterPoids',component: ConsulterPoidsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
