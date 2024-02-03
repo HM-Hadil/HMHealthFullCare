a58d96be4067e73488eb041ab63c233fb8ec5178
HM-Hadil <hadilhammami670@gmail.com>
2024-02-03 01:38:09 +0100

    adding style to sticky navbar

--- src/app/Home/header/header.component.ts
+++ src/app/Home/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,HostListener  } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  isSticky: boolean = false;
+
+  @HostListener('window:scroll', ['$event'])
+  checkScroll() {
+    this.isSticky = window.pageYOffset > 0;
+  }
 
 }
