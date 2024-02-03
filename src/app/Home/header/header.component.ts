dd0cedaa7e270e99654aa16a3bcb4d170ebde1bf
HM-Hadil <hadilhammami670@gmail.com>
2024-02-03 02:45:27 +0100

    adding responsive  media query

--- src/app/Home/header/header.component.ts
+++ src/app/Home/header/header.component.ts
@@ -1,16 +1,19 @@
-import { Component,HostListener  } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
   isSticky: boolean = false;
+  isMenuOpen: boolean = false;
 
   @HostListener('window:scroll', ['$event'])
   checkScroll() {
     this.isSticky = window.pageYOffset > 0;
   }
-
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
